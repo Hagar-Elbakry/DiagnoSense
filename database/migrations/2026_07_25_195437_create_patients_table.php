7ddac6cef74d991bc5d19c80846b34312aedf526
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-07-25 22:55:37 +0300

    create migrations for doctors and patients tables

---
+++ database/migrations/2026_07_25_195437_create_patients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->date('date_of_birth');
+            $table->enum('gender', ['male', 'female'])->nullable();
+            $table->string('national_id')->unique()->index()->nullable();
+            $table->enum('status', ['under review', 'critical', 'stable'])->default('under review');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
