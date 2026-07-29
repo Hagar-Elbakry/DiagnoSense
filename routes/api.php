69c2e2f6bc6ceab3d4f217c609cfe25ff444d978
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-07-29 19:25:38 +0300

    feat: reorganize authentication routes and ensure user type check middleware is applied

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,15 @@ Route::prefix('v1')->group(function (){
     Route::prefix('auth')->group(function () {
         Route::post('register', [AuthenticationController::class, 'register'])->name('auth.register');
         Route::controller(SocialAuthController::class)->prefix('google')->as('google.')->group(function () {
-        Route::get('/redirect', 'redirectToGoogle')->name('redirect');
-        Route::get('/callback', 'handleGoogleCallback')->name('callback');
-    });
+            Route::get('/redirect', 'redirectToGoogle')->name('redirect');
+            Route::get('/callback', 'handleGoogleCallback')->name('callback');
+        });
+
+        Route::middleware('check-user-type')->group(function () {
+            Route::controller(AuthenticationController::class)->group(function () {
+                Route::post('/login/{type}','login')->name('login')->middleware('throttle:login');
+                Route::post('/logout/{type}', 'logout')->name('logout')->middleware('auth:sanctum');
+            });
+        });
     });
 });
