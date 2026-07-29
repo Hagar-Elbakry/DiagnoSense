95abc145d5a79e297913077f25f6e19b671180d0
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-07-29 19:15:58 +0300

    feat: add user relationship method to UserSocialAccount model

--- app/Models/UserSocialAccount.php
+++ app/Models/UserSocialAccount.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\User;
 
 class UserSocialAccount extends Model
 {
     protected $fillable = ['user_id', 'provider', 'provider_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(related: User::class);
+    }
 }
