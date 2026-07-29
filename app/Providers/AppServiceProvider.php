3db9b4843db0e636e9472d7ac3b479e181c737c6
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-07-29 19:25:08 +0300

    feat: implement rate limiting for login attempts with custom error response

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,10 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Mail;
 use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
 use Symfony\Component\Mailer\Transport\Dsn;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use App\Helpers\ApiResponse;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +26,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        RateLimiter::for('login', function ($request) {
+            return Limit::perMinute(5)->by($request->contact.$request->ip())
+                ->response(function (Request $request, array $headers) {
+                    return ApiResponse::error(
+                        message: 'Too many login attempts. Retry after '.$headers['Retry-After'].' seconds.',
+                        status: 429
+                    );
+                });
+        });
+
         Mail::extend('brevo', function () {
             return (new BrevoTransportFactory())->create(
                 Dsn::fromString(config('services.brevo.dsn'))
