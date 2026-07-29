7f9b27a4cc8e3f3cf6b7ad39cb165e700ed92231
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-07-29 22:27:25 +0300

    feat: add VerifyOtpRequest for OTP validation in password recovery

---
+++ app/Http/Requests/Auth/VerifyOtpRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+
+use App\Rules\ValidContactRule;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class VerifyOtpRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $type = $this->route('type');
+        return [
+            'contact' => [
+                'required',
+                'string',
+                new ValidContactRule,
+                Rule::exists('users', 'contact')->where(function ($query) use ($type) {
+                    $query->where('type', $type);
+                })
+            ],
+            'otp' => ['required', 'string', 'size:6'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'contact.required' => 'Contact is required.',
+            'contact.exists'   => 'This contact is invalid.',
+        ];
+    }
+}
