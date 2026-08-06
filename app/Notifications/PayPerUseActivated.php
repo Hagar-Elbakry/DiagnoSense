10809c89b28a7d9bc040bf991ad43f33d8b99c11
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-08-06 16:41:51 +0300

    Add PayPerUseActivated notification for pay-per-use billing system

---
+++ app/Notifications/PayPerUseActivated.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Notification;
+
+class PayPerUseActivated extends Notification implements ShouldBroadcast
+{
+    use Queueable;
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database', 'broadcast'];
+    }
+
+    public function toDatabase(object $notifiable): array
+    {
+        return [
+            'title' => 'Pay-per-use system activated',
+            'message' => 'Now you can enjoy the flexibility of our pay-per-use billing system.',
+        ];
+    }
+
+    public function toBroadcast(object $notifiable): BroadcastMessage
+    {
+        return new BroadcastMessage([
+            'title' => 'Pay-per-use system activated',
+            'message' => 'Now you can enjoy the flexibility of our pay-per-use billing system.',
+        ]);
+    }
+}
