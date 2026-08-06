c743adea2b24322e11e08974765c65b3dfb84ec2
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-08-06 16:41:21 +0300

    Add subscription endpoint for plan-based subscriptions

--- routes/api.php
+++ routes/api.php
@@ -47,6 +47,7 @@ Route::prefix('v1')->group(function () {
 
         Route::controller(SubscriptionController::class)->prefix('subscriptions')->as('subscriptions.')->group(function(){
             Route::post('/{plan}/subscribe', 'store')->name('subscribe');
+            Route::post('pay-per-use', 'switchToPayPerUse')->name('pay-per-use');
         });
     });
 });
