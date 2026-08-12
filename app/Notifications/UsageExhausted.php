967dde6359404dd98bd9c79628c541e658e24f47
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-08-12 14:06:11 +0300

    Add UsageExhausted and UsageThresholdReached notification classes for usage alerts

---
+++ app/Notifications/UsageExhausted.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Notification;
+
+class UsageExhausted extends Notification implements ShouldBroadcast
+{
+    use Queueable;
+
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database', 'broadcast'];
+    }
+
+    public function toDatabase(object $notifiable): array
+    {
+        return [
+            'title' => 'Usage Limit Reached',
+            'message' => 'You have used all your available summaries. Please top up or upgrade to continue.',
+        ];
+    }
+
+    public function toBroadcast(object $notifiable): BroadcastMessage
+    {
+        return new BroadcastMessage([
+            'title' => 'Usage Limit Reached',
+            'message' => 'You have used all your available summaries. Please top up or upgrade to continue.',
+        ]);
+    }
+}
---
+++ app/Notifications/UsageThresholdReached.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Notification;
+
+class UsageThresholdReached extends Notification implements ShouldBroadcast
+{
+    use Queueable;
+
+    public function __construct(
+        protected int|float $percentage = 80,
+    ) {}
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database', 'broadcast'];
+    }
+
+    public function toDatabase(object $notifiable): array
+    {
+        return [
+            'title' => "Usage Alert: {$this->percentage}% Reached",
+            'message' => "You have consumed {$this->percentage}% of your plan's summaries. Top up soon to avoid interruption.",
+        ];
+    }
+
+    public function toBroadcast(object $notifiable): BroadcastMessage
+    {
+        return new BroadcastMessage([
+            'title' => "Usage Alert: {$this->percentage}% Reached",
+            'message' => "You have consumed {$this->percentage}% of your plan's summaries. Top up soon to avoid interruption.",
+        ]);
+    }
+}
