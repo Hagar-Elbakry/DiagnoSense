332b857b68d51a3b33da204a9667a2c16ac66661
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-08-12 15:36:09 +0300

    Refactor ReportService to streamline report insertion and remove redundant storeReport method

--- app/Services/ReportService.php
+++ app/Services/ReportService.php
@@ -2,21 +2,26 @@
 
 namespace App\Services;
 
-use App\Models\Patient;
-use Illuminate\Support\Str;
 use App\Helpers\FileSystem;
+use App\Models\Patient;
+use App\Models\Report;
+use Illuminate\Support\Str;
 use Exception;
 
 class ReportService
 {
+
     public function getPathsForAI(array $reportsTypes, array $data, Patient $patient, array $pathsForAI): array
     {
         try {
+            $reportsToInsert = [];
             foreach ($reportsTypes as $type) {
                 if (! empty($data[$type])) {
-                    $pathsForAI = $this->processAndStoreFile($data[$type], $type, $patient, $pathsForAI);
+                    [$pathsForAI, $reportsForType] = $this->processAndStoreFile($data[$type], $type, $patient, $pathsForAI);
+                    $reportsToInsert = array_merge($reportsToInsert, $reportsForType);
                 }
             }
+            Report::insert($reportsToInsert);
         } catch (Exception $e) {
             foreach ($pathsForAI as $paths) {
                 foreach ($paths as $path) {
@@ -31,6 +36,7 @@ class ReportService
 
     private function processAndStoreFile(array $data, string $type, Patient $patient, array $pathsForAI): array
     {
+        $reportsToInsert = [];
         foreach ($data as $file) {
             $fileName = $file->getClientOriginalName();
             $uniqueName = time().'_'.Str::random(5).'.'.$fileName;
@@ -39,20 +45,17 @@ class ReportService
                 throw new Exception("Failed to upload $fileName file to azure blob storage.");
             }
             $mimeType = $file->getMimeType();
-            $this->storeReport($patient, $type, $fileName, $filePath, $mimeType);
+            $reportsToInsert[] = [
+                'type' => $type,
+                'file_name' => $fileName,
+                'file_path' => $filePath,
+                'mime_type' => $mimeType,
+                'patient_id' => $patient->id,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
             $pathsForAI[$type][] = $filePath;
         }
-
-        return $pathsForAI;
-    }
-
-    private function storeReport(Patient $patient, string $type, string $fileName, string $filePath, string $mimeType): void
-    {
-        $patient->reports()->create([
-            'type' => $type,
-            'file_name' => $fileName,
-            'file_path' => $filePath,
-            'mime_type' => $mimeType,
-        ]);
+        return [$pathsForAI, $reportsToInsert];
     }
 }
