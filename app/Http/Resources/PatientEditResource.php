6b4978aae42831107ed06a841c7716ba4567736a
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-08-17 20:38:20 +0300

    feat: add patient edit resource with medical history and files

---
+++ app/Http/Resources/PatientEditResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Helpers\FileSystem;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\MedicalHistoryResource;
+
+class PatientEditResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'personal_info' => [
+                'name' => $this->user->name,
+                'contact' => $this->user->contact,
+                'date_of_birth' => $this->date_of_birth->format('Y-m-d'),
+                'gender' => $this->gender,
+                'national_id' => $this->national_id,
+            ],
+            'medical_history' => new MedicalHistoryResource($this->medicalHistory),
+            'existing_files' => $this->reports->map(fn ($report) => [
+                'id' => $report->id,
+                'type' => $report->type,
+                'name' => $report->file_name,
+                'url' => FileSystem::getTempUrl($report->file_path),
+            ]),
+        ];
+    }
+}
