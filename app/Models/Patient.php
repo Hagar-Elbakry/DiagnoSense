42490393ba41dd375bd31cd5d8b31d623cbf875b
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-08-25 03:01:14 +0300

    feat: add PatientStatus enum and method to retrieve statuses in Patient model

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Enums\PatientStatus;
 
 class Patient extends Model
 {
@@ -80,5 +81,10 @@ class Patient extends Model
     public function labResults(): HasMany
     {
         return $this->hasMany(PatientLabResult::class);
+    }
+
+    public static function getStatuses(): array
+    {
+        return PatientStatus::cases();
     }
 }
