573a2c4881038d9d4034072d67490c053a0d7424
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-08-27 00:55:57 +0300

    feat: implement StoreTaskAction and StoreVisitRequirementAction for task management

---
+++ app/Actions/StoreVisitRequirementAction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\Visit;
+
+
+abstract class StoreVisitRequirementAction
+{
+    protected function updateVisitIfNeeded(Visit $visit, array $data): void
+    {
+        if (! $visit->next_visit_date && isset($data['next_visit_date'])) {
+            $visit->update(['next_visit_date' => $data['next_visit_date']]);
+        }
+        if ($data['action'] == 'save') {
+            $visit->update(['status' => 'completed']);
+        }
+    }
+}
