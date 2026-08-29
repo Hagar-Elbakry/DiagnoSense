ee6cf79ca5c8289249fe89cc9b687b69b9b19877
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-08-29 18:51:39 +0300

    feat: add CompleteTaskRequest for task completion authorization

---
+++ app/Http/Requests/CompleteTaskRequest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Requests\Task;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CompleteTaskRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        $task = $this->route('task');
+
+        return $task->visit?->patient_id === $this->user()->patient->id;
+    }
+}
